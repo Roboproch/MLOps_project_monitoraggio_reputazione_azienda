f30919dfc7d55c0fdbc5759756baafc95b809a94
Roboproch <davide.proch@gmail.com>
2026-03-16 22:52:44 +0000

    import del dataset nel file python del modello

--- MLOps_project/modello.py
+++ MLOps_project/modello.py
@@ -1,3 +1,11 @@
 from transformers import pipeline
 sentiment_task = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest", tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest")
-print(sentiment_task("Covid cases are increasing fast!"))
+# print(sentiment_task("Covid cases are increasing fast!"))
+
+from datasets import load_dataset
+ds = load_dataset("SetFit/tweet_sentiment_extraction")
+# il dataset viene splittato da load_dataset
+# stampa il primo record del set di train
+print(ds['train'][0])
+# stampa il primo record del set di test
+print(ds['test'][0])
