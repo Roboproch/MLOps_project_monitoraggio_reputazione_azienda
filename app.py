f61df04aca44d793f56a742711d0683158e19e02
Roboproch <davide.proch@gmail.com>
2026-03-25 03:25:47 +0000

    mod app.py pro gradio

--- app.py
+++ app.py
@@ -9,14 +9,26 @@ dataset = LoadDataset()
 
 X = dataset.X
 y = dataset.y
+y_pred = model.predict(X)
 
-def predict(data) :
-    return model.predict(data)
+acc = f"{accuracy_score(y, y_pred)}"
+
+valori = ['negative', 'neutral', 'positive']
+
+def predict_tweet(tweet,esito_atteso) :
+    global acc
+    X.append(tweet)
+    y.append(esito_atteso)
+    y_new = model.predict(tweet)[0]
+    y_pred.append(y_new)
+    acc = f"{accuracy_score(y, y_pred)}"
+    return y_new,acc
 
 demo = gr.Interface(
-    fn=predict,
-    inputs='text_in',
-    outputs='text_out'
+    fn=predict_tweet,
+    inputs=[gr.Textbox(label="Tweet"),gr.Dropdown(choices=valori,label="Esito atteso")],
+    outputs=[gr.Textbox(label="Previsione modello"),gr.Textbox(label="Ricalcolo accuracy")],
+    flagging_mode = 'never'
 )
 
 demo.launch()
