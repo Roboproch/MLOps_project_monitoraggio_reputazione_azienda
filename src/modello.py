5f2968612ee7b4fce4c76ac93d8adf293f3d08c3
Roboproch <davide.proch@gmail.com>
2026-03-26 15:26:07 +0100

    Commenti modello.py
    Commenti modello.py

--- src/modello.py
+++ src/modello.py
@@ -2,13 +2,14 @@ import torch
 from transformers import pipeline
 
 class Modello :
+    # Gestisce l'import del modello da Hugging Face e implementa un metodo per la predizione del sentiment
 
     def __init__(self) :
         # Import del modello da Hugging Face
         self.sentiment_task = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest", tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest")
 
     def predict(self,tweets) :
-        # Metodo per le predizioni, prende in input una o più stringhe
+        # Metodo per le predizioni, prende in input una o più stringhe (sottoforma di lista eventualmente)
         # Definisco un batch_size per efficienza
         results = self.sentiment_task(tweets, batch_size=32) 
         return [res["label"] for res in results]
