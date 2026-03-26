414cbcbac68d8a65f8ba7ef9f0615830cce460d6
Roboproch <davide.proch@gmail.com>
2026-03-26 16:17:54 +0100

    Commenti app.py

--- app.py
+++ app.py
@@ -4,21 +4,16 @@ from src.dataset import LoadDataset
 from sklearn.metrics import accuracy_score
 import gradio as gr
 
-#model = Modello()
-#dataset = LoadDataset()
+# Dichiaro le variabili e costanti che mi serviranno in seguito
 model = None
 dataset = None
-
 X = None
 y = None
-#X = dataset.X
-#y = dataset.y
-# y_pred = model.predict(X)
-y_pred   = ['negative', 'neutral', 'positive','negative', 'neutral', 'positive','negative', 'neutral', 'positive','positive']
-
+y_pred = None
 valori = ['negative', 'neutral', 'positive']
 
 def predict_tweet(tweet,esito_atteso) :
+    # Aggiunge gli input (tweet ed esito_atteso) e l'esito predetto al dataset; restituisce l'esito predetto e la nuova accuracy calcolata sul dataset aggiornato
     X.append(tweet)
     y.append(esito_atteso)
     y_new = model.predict(tweet)[0]
@@ -26,15 +21,17 @@ def predict_tweet(tweet,esito_atteso) :
     acc_new = f"{accuracy_score(y, y_pred)}"
     return y_new,acc_new
 
-
-
 if __name__ == "__main__":
-    print("Avvio caricamento componenti...")
+    # Inizializza gli oggetti
     model = Modello()
     dataset = LoadDataset()
     X = dataset.X
     y = dataset.y
+    y_pred = model.predict(X)
 
+    # Definisce l'interfaccia grafica di Gradio
+    # Prende in input un testo (tweet) e il sentiment predetto (quest'ultimo scelto da un menu a tendina)
+    # Restituisce l'esito predetto e la nuova accuracy calcolata sul dataset aggiornato
     demo = gr.Interface(
         fn=predict_tweet,
         inputs=[gr.Textbox(label="Tweet"),gr.Dropdown(choices=valori,label="Esito atteso")],
@@ -42,5 +39,5 @@ if __name__ == "__main__":
         flagging_mode = 'never'
     )
 
-    print("Lancio")
+    # Lancia l'interfaccia grafica
     demo.launch()
